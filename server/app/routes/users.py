f3e0eca68bb796bf1b71f1d7bba683f7366ef4b2
seatangle77 <seatangle@outlook.com>
2025-06-20 17:59:11 +0800

    调试a53

--- server/app/routes/users.py
+++ server/app/routes/users.py
@@ -20,7 +20,7 @@ async def get_users():
     print("🟢 [get_users] API 已被触发")
     # 🧪 使用写死 token 测试 FCM 推送
     try:
-        token = "exi6Sk9qRiCLuQgOaSGWv3:APA91bGIc7beHBH9khzTQz0G45S5tH9ZI9blkUs8aWc6ra7eB_ekpMO5g-H5TFVZ7VjbOvOWUIvgZ1gkeRjp3Uk3UOhuCqunpmdeIe7u4LM9zR2MnWG3EdY"
+        token = "fwffQyoSR9iNtfKB888iFM:APA91bGY-WUkWenyCvfgBQExQktCpqzjOs78TwbWTSrM9idz1g00OJlL38XQL20fbBiYq8ewn7vg8JXGFP8vmBsujEUw7vFE8KgZ6SYBSnLtEFS_jtOarZA"
         print(f"🧪 使用写死 token: {token}")
         message = messaging.Message(
             notification=messaging.Notification(
